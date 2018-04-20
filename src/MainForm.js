81e7bf107532200b40734cf100360355fa26d079
Станислав Божевский <s.bogevskiy@into-sana.ua>
2018-04-20 18:27:08 +0300

    working sending to the server

--- src/MainForm.js
+++ src/MainForm.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import './MainForm.css';
 import {
 		Col,
+		Row,
+		Container,
 		Button,
 		Form,
 		FormGroup,
@@ -10,35 +12,85 @@ import {
 		FormFeedback,
 		ButtonGroup
 } from 'reactstrap';
+import MaskedInput from 'react-text-mask'
+
+
+const inputParsers = {
+  date(input) {
+    const [month, day, year] = input.split('/');
+    return `${year}-${month}-${day}`;
+  },
+  uppercase(input) {
+    return input.toUpperCase();
+  },
+  number(input) {
+    return parseFloat(input);
+  },
+};
+
+const API = 'http://192.168.100.190/Work/hs/BonusCards/'
+const CREATE_CARD = 'createcard/'
 
 class MainForm extends Component {
 
 		constructor(props) {
 				super(props)
 				this.state = {
-						inputName: '',
-						inputPhone: '',
-						inputBirthdate: '',
-						emptyName: false,
-						emptyPhone: false,
-						emptyBirthdate: false,
-						emptyCardNumber: false,
-						emptyPromoNumber: false,
+						inputName: 'Чапай',
+						inputPhone: '+39 (093) 234-34-23',
+						inputBirthdate: '1986-04-12',
 						promoDisabled: true,
-						cardNumber: "",
-						promoNumber: ""
+						cardNumber: "11111",
+						promoNumber: "123123",
 				}
 
-				this.submitData = this
-						.submitData
-						.bind(this);
-
-				this.onRadioBtnClick = this
-						.onRadioBtnClick
-						.bind(this);
-
+				this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
+				this.handleSubmit = this.handleSubmit.bind(this);
 		}
 
+		handleSubmit(event) {
+
+			if (!event.target.checkValidity()) {
+				// form is invalid! so we do nothing
+				return;
+			}
+
+			event.preventDefault();
+
+			let urlEncodedData = "";
+			let urlEncodedDataPairs = [];
+
+			// Turn the data object into an array of URL-encoded key/value pairs.
+			for(let name in this.state) {
+				urlEncodedDataPairs.push(name + '=' + this.state[name]);
+			}
+			urlEncodedData = urlEncodedDataPairs.join('&');
+
+			// for (let name of data.keys()) {
+			// 	const input = form.elements[name];
+			// 	const parserName = input.dataset.parse;
+	
+			// 	if (parserName) {
+			// 		const parser = inputParsers[parserName];
+			// 		const parsedValue = parser(data.get(name));
+			// 		data.set(name, parsedValue);
+			// 	}
+			// }
+
+			fetch(API + CREATE_CARD,{
+				method: 'POST',
+				headers: {  
+					'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8" 
+				},  
+				body: urlEncodedData, })
+			.then(response => {
+					return response.text();
+			})
+			.then(data => console.log(data))
+			.catch(error => console.log(error));
+		}
+
+	
 		handleUserInput = (e) => {
 				const name = e.target.name;
 				const value = e.target.value;
@@ -53,59 +105,27 @@ class MainForm extends Component {
 				this.setState({promoDisabled: promoState, promoNumber: ""});
 		}
 
-		submitData(e) {
-				if (this.state.inputName === "") {
-						this.setState({emptyName: true});
-				} else {
-						this.setState({emptyName: false});
-				}
-
-				if (this.state.inputPhone === "") {
-						this.setState({emptyPhone: true});
-				} else {
-						this.setState({emptyPhone: false});
-				}
-
-				if (this.state.inputBirthdate === "") {
-						this.setState({emptyBirthdate: true});
-				} else {
-						this.setState({emptyBirthdate: false});
-				}
-
-				if (this.state.cardNumber === "") {
-						this.setState({emptyCardNumber: true});
-				} else {
-						this.setState({emptyCardNumber: false});
-				}
-
-				if (this.state.promoNumber === "" && this.state.promoDisabled) {
-						this.setState({emptyPromoNumber: true});
-				} else {
-						this.setState({emptyPromoNumber: false});
-				}
-				//console.log(this.refs.codeblock.promoNumber) // "qux"
-		}
-
 		render() {
 				return (
 
-						<div className="container mainForm">
+						<Container className="mainForm">
 
-								<div className="row">
-										<div className="col"></div>
+								<Row>
+										<Col></Col>
 
-										<div className="col-6">
+										<Col xs="6">
 
-												<Form>
+												<Form className="needs-validation" noValidate onSubmit={this.handleSubmit}>
 														<FormGroup >
 																<Label for="inputName">ФИО</Label>
 																<Input
 																		type="text"
+																		pattern="[а-яА-Я ]+"
 																		className="form-control"
 																		name="inputName"
 																		placeholder="Чапаев Василий Иванович"
+																		required
 																		value={this.state.inputName}
-																		invalid={this.state.emptyName}
 																		onChange={this.handleUserInput}/>
 																<FormFeedback>Укажите ФИО клиента.</FormFeedback>
 														</FormGroup>
@@ -114,14 +134,17 @@ class MainForm extends Component {
 																<Col>
 
 																		<Label for="inputPhone" className="col-md-6">Телефон</Label>
-																		<Input
-																				type="phone"
+																		<MaskedInput className="form-control"
+																				type="tel"
+																				mask={['+', '3', '8', '0', ' ', '(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/]}
+																				guide={true}
+																				showMask={true}
+																				placeholder="+380 (__) ___-__-__"
 																				name="inputPhone"
-																				autoComplete="off"
-																				placeholder="+380 XX XXX XX XX"
-																				value={this.state.inputPhone}
-																				invalid={this.state.emptyPhone}
-																				onChange={this.handleUserInput}/>
+																				pattern="((\+380 )\(\d{2}\)) \d{3}-\d{2}-\d{2}"
+																				required
+																			  value={this.state.inputPhone}
+																			  onChange={this.handleUserInput}/> 
 																		<FormFeedback>Укажите телефон клиента.</FormFeedback>
 																</Col>
 
@@ -133,8 +156,8 @@ class MainForm extends Component {
 																				type="date"
 																				name="inputBirthdate"
 																				placeholder="password placeholder"
+																				required
 																				value={this.state.inputBirthdate}
-																				invalid={this.state.emptyBirthdate}
 																				onChange={this.handleUserInput}/>
 																		<FormFeedback>Укажите дату рождения клиента.</FormFeedback>
 																</Col>
@@ -163,9 +186,10 @@ class MainForm extends Component {
 																				<Input
 																						type="number"
 																						name="cardNumber"
+																						required
 																						value={this.state.cardNumber}
 																						onChange={this.handleUserInput}
-																						invalid={this.state.emptyCardNumber}/>
+																						/>
 																				<FormFeedback>Укажите код карты.</FormFeedback>
 																		</Col>
 																		< Col >
@@ -173,10 +197,11 @@ class MainForm extends Component {
 																				<Input
 																						type="number"
 																						name="promoNumber"
+																						required
 																						value={this.state.promoNumber}
 																						onChange={this.handleUserInput}
 																						disabled={this.state.promoDisabled}
-																						invalid={this.state.emptyPromoNumber}/>
+																						/>
 																				<FormFeedback>Укажите промокод.</FormFeedback>
 																		</Col>
 
@@ -190,16 +215,17 @@ class MainForm extends Component {
 																		color="primary"
 																		size="lg"
 																		block
-																		onClick={this.submitData}>Отправить</Button>
+																		type="submit"
+																		>Отправить</Button>
 
 														</FormGroup>
 												</Form>
 
-										</div>
-										<div className="col"></div>
-								</div>
+										</Col>
+										<Col></Col>
+								</Row>
 
-						</div>
+						</Container>
 				);
 		}
 }
